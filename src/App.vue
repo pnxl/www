b3ce11a9923ff4eae04e68d44187073141a1e0f1
Jason <development@pnxl.dev>
2024-06-01 17:25:39 +0700

    styles: add border under header

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     class="min-h-screen min-w-full bg-neutral-100 text-neutral-700 dark:bg-black dark:text-neutral-200 p-4"
   >
     <Header />
+    <hr class="my-4 border-neutral-500 rounded-full" />
     <router-view v-slot="{ Component }">
       <transition name="fade" mode="out-in">
         <component :is="Component" />
