3efc8c0ee3fe884f8ca56d8fd4353ca30b9fdb1a
Jason <development@pnxl.dev>
2024-06-02 10:41:21 +0700

    styles: add background blur behind

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,33 +1,38 @@
 <template>
-  <header
-    class="flex flex-row justify-between md:text-5xl sm:text-4xl text-3xl font-semibold"
+  <div
+    class="absolute w-full -mx-4 px-4 pt-4 z-50 bg-neutral-100/65 dark:bg-neutral-950/85 backdrop-blur-xl"
   >
-    <RouterLink
-      to="/"
-      :class="
-        $route.fullPath === '/'
-          ? 'italic-active opacity-100'
-          : 'hover:opacity-100 opacity-65 italic-on-hover'
-      "
-      >Home</RouterLink
+    <header
+      class="flex flex-row justify-between md:text-5xl sm:text-4xl text-3xl font-semibold"
     >
-    <RouterLink
-      to="/about"
-      :class="
-        $route.fullPath === '/about'
-          ? 'italic-active opacity-100'
-          : 'hover:opacity-100 opacity-65 italic-on-hover'
-      "
-      >About</RouterLink
-    >
-    <RouterLink
-      to="/portfolio"
-      :class="
-        $route.fullPath === '/portfolio'
-          ? 'italic-active opacity-100'
-          : 'hover:opacity-100 opacity-65 italic-on-hover'
-      "
-      >Portfolio</RouterLink
-    >
-  </header>
+      <RouterLink
+        to="/"
+        :class="
+          $route.fullPath === '/'
+            ? 'italic-active opacity-100'
+            : 'hover:opacity-100 opacity-65 italic-on-hover'
+        "
+        >Home</RouterLink
+      >
+      <RouterLink
+        to="/about"
+        :class="
+          $route.fullPath === '/about'
+            ? 'italic-active opacity-100'
+            : 'hover:opacity-100 opacity-65 italic-on-hover'
+        "
+        >About</RouterLink
+      >
+      <RouterLink
+        to="/portfolio"
+        :class="
+          $route.fullPath === '/portfolio'
+            ? 'italic-active opacity-100'
+            : 'hover:opacity-100 opacity-65 italic-on-hover'
+        "
+        >Portfolio</RouterLink
+      >
+    </header>
+    <hr class="my-4 border-neutral-500 rounded-full opacity-50" />
+  </div>
 </template>
